5dc3736a1b0c6e9c9e475861c223eca9fa0a83fb
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-11-07 13:41:15 +0100

    feat: GH action install

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const client = require('@nemerosa/ontrack-github-actions-module-install');
+
+(async () => {
+    try {
+        await setup();
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
+
+async function setup() {
+
+    // Installing and configuring the CLI
+    const {version} = await client.install({
+        version: core.getInput('version'),
+        githubToken: core.getInput('github-token'),
+        acceptDraft: false,
+        logging: true,
+        yontrackUrl: core.getInput('url'),
+        yontrackToken: core.getInput('token'),
+        connRetryCount: core.getInput('conn-retry-count'),
+        connRetryWait: core.getInput('conn-retry-wait'),
+    })
+
+    core.setOutput('installed', version);
+    core.info(`Yontrack CLI version ${version} installed`);
+}
