5772db5b295ee8c9f76aba32021f1a3006386f4c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2025-11-08 10:03:31 +0100

    feat: using environment variables as defaults for the URL and token

--- index.js
+++ index.js
@@ -11,14 +11,23 @@ const client = require('@nemerosa/ontrack-github-actions-module-install');
 
 async function setup() {
 
+    let url = core.getInput('url');
+    if (!url) {
+        url = process.env.YONTRACK_URL;
+    }
+    let token = core.getInput('token');
+    if (!token) {
+        token = process.env.YONTRACK_TOKEN;
+    }
+
     // Installing and configuring the CLI
     const {version, dir} = await client.install({
         version: core.getInput('version'),
         githubToken: core.getInput('github-token'),
         acceptDraft: false,
         logging: true,
-        yontrackUrl: core.getInput('url'),
-        yontrackToken: core.getInput('token'),
+        yontrackUrl: url,
+        yontrackToken: token,
         yontrackUser: core.getInput('cli-config'),
         connRetryCount: core.getInput('conn-retry-count'),
         connRetryWait: core.getInput('conn-retry-wait'),
