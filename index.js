c679bec1b3963a374013ff459a595ce9cb8b6ee4
Steven Rathbauer <steven.rathbauer@collibra.com>
2026-05-05 22:30:22 -0400

    refactor: convert index.js to DI pattern with dynamic ESM import for @actions/core

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const core = require('@actions/core');
 const client = require('@nemerosa/ontrack-github-actions-module-install');
 
-(async () => {
-    try {
-        await setup();
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-})();
-
-async function setup() {
-
+async function runAction({ core, client: clientDep }) {
     let url = core.getInput('url');
     if (!url) {
         url = process.env.YONTRACK_URL;
@@ -20,8 +10,7 @@ async function setup() {
         token = process.env.YONTRACK_TOKEN;
     }
 
-    // Installing and configuring the CLI
-    const {version, dir} = await client.install({
+    const { version, dir } = await clientDep.install({
         version: core.getInput('version'),
         githubToken: core.getInput('github-token'),
         acceptDraft: false,
@@ -31,9 +20,22 @@ async function setup() {
         yontrackUser: core.getInput('cli-config'),
         connRetryCount: core.getInput('conn-retry-count'),
         connRetryWait: core.getInput('conn-retry-wait'),
-    })
+    });
 
     core.setOutput('installed', version);
-    core.addPath(dir)
+    core.addPath(dir);
     core.info(`Yontrack CLI version ${version} installed`);
+}
+
+module.exports = { runAction };
+
+if (process.env.NODE_ENV !== 'test') {
+    (async () => {
+        const core = await import('@actions/core');
+        try {
+            await runAction({ core, client });
+        } catch (error) {
+            core.setFailed(error.message);
+        }
+    })();
 }
